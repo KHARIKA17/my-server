a822eb152fa88b609fb1b2edd10a112a911eebcd
KHARIKA17 <60010885+KHARIKA17@users.noreply.github.com>
2020-03-24 16:54:43 -0500

    edited

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ const server = http.createServer((req, res) => {
 // use the server console to tell user where to find the server
 // use backticks for template literals with embedded expressions
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://localhost:${port}/`)
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
